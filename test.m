4163c3bc234151cbc283305707bdba9915df417f
Johan <johaa1993@gmail.com>
2016-04-08 00:59:13 +0200

    test

--- test.m
+++ test.m
@@ -1,3 +1,6 @@
-emptyvec = nan(1, 10);
-h1 = plot(emptyvec, emptyvec, emptyvec, emptyvec,'-bo');
-a = h1(1).XData(1);
+ax1 = subplot(2,1,1);
+Z = peaks;
+plot(ax1,Z(1:20,:))
+
+ax2 = subplot(2,1,2);
+plot(ax2,Z)
